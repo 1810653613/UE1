9c9b3d4a6755b9332300aabed28f659cfde78d4f
Joachimstaller Maximilian <1810653613@stud.fh-kufstein.ac.at>
2018-10-11 11:22:30 +0200

    first Commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String Vorname = "Max";
         String Nachname = "Joachimstaller";
         int Geburtsjahr = (int) 1997;
-        System.out.println("Hello "+Nachname+" you´re born in "+Geburtsjahr);
+        System.out.println("Hello "+Vorname+" "+Nachname);
     }
 }
