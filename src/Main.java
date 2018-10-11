7d7a288e14944761f418262c30865661ab39aa69
Joachimstaller Maximilian <1810653613@stud.fh-kufstein.ac.at>
2018-10-11 11:03:44 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String vorname = "Michael";
+        int alter = (int) 21.7;
+        System.out.println("Hallo "+vorname+" du bist "+alter);
+    }
+}
